b64ec556c6e9d58ab712c03dbf6a35c9bc7c9aa8
DaveDV <dave.devolder@student.kdg.be>
2013-02-08 10:02:55 +0100

    RestUserController: getFund hernoemd naar getUser

--- src/main/java/be/kdg/controller/RestUserController.java
+++ src/main/java/be/kdg/controller/RestUserController.java
@@ -3,15 +3,9 @@ package be.kdg.controller;
 import be.kdg.model.User;
 import be.kdg.service.UserService;
 import org.apache.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Author: Ben Oeyen
@@ -21,16 +15,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class RestUserController {
-    private static final Logger logger= Logger.getLogger(RestUserController.class);
+    private static final Logger logger = Logger.getLogger(RestUserController.class);
 
     @RequestMapping(value = "/rest/users/{userId}", method = RequestMethod.GET)
-    public void getFund(@PathVariable("userId") String userId) {
+    public void getUser(@PathVariable("userId") String userId) {
         User user = null;
 
         // validate input
         if (userId.isEmpty() || userId.length() < 5) {
             String sMessage = "Error invoking getFund - Invalid fund Id parameter";
-           // return createErrorResponse(sMessage);
+            // return createErrorResponse(sMessage);
         }
 
         try {
