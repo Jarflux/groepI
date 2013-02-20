59b4c5279103e6371d42fa26eeef6a1cec322de0
Vincent Verbist <vincentjanv@gmail.com>
2013-02-20 09:44:19 +0100

    Mail adres veranderd.

--- src/main/java/be/kdg/groepi/service/UserService.java
+++ src/main/java/be/kdg/groepi/service/UserService.java
@@ -172,7 +172,7 @@ public class UserService {
         TripMail tim = (TripMail) context.getBean("tripMail");
 
         //TODO: can't connect to host
-        tim.sendMail("info@trippie.be", "vincentjanv@gmail.com", "Subject?",
-                "text\n\nHEY MEZELF WAT ZIE JE ER GOED UIT\n" + newPassword);
+        tim.sendMail("info@trippie.be", "info@trippie.be", "Subject?",
+                "Je nieuw wachtwoord is:\n" + newPassword);
     }
 }
