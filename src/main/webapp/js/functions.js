64bb70410d52381a003fb2047ec0cb081fbc908e
Vincent Verbist <vincentjanv@gmail.com>
2013-02-21 14:29:08 +0100

    Nieuwe pom

--- src/main/webapp/js/functions.js
+++ src/main/webapp/js/functions.js
@@ -28,9 +28,9 @@ function preparetooltips()
     function validateform()
     {
         $(".dateregister").dateinput({format: 'yyyy/mm/dd',yearRange: [-80, -12],firstDay: 1,selectors: true,value: "Today"  });
-        $(".date").dateinput({format: 'yyyy/mm/dd',yearRange: [0, 1],firstDay: 1,selectors: true,value: "Today"  });
+        $(".date").dateinput({format: 'yyyy-mm-dd',yearRange: [0, 1],firstDay: 1,selectors: true,value: "Today"  });
 
-            $("form.validate input").each(function()
+            $("form.validate input, form.validate textarea").each(function()
         {
            $(this).after("<span class='deerror'></span>");
             /* Required validatie */
