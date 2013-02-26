9c0d503e3ed7238939e52cea37423d67230b637d
JarFlux <ben.oeyen@gmail.com>
2013-02-26 14:44:38 +0100

    Added myprofile/edit

--- src/main/java/be/kdg/groepi/controller/RestUserController.java
+++ src/main/java/be/kdg/groepi/controller/RestUserController.java
@@ -14,22 +14,22 @@ import java.util.Calendar;
  * Author: Ben Oeyen
  * Date: 7/02/13
  * Class: User REST Controller
- * Description:  Controller to handle REST service calls
+ * Description: Controller to handle REST service calls
  */
-
 @Controller
 @RequestMapping("profile")
 public class RestUserController {
+
     private static final Logger logger = Logger.getLogger(RestUserController.class);
 
     @RequestMapping(value = "/view/{userId}", method = RequestMethod.GET)
     public ModelAndView getUser(@PathVariable("userId") String userId) {
         User user;
         // validate input
-        /*if (userId.isEmpty() || userId.length() < 5) {
-            String sMessage = "Error invoking getFund - Invalid fund Id parameter";
-            // return createErrorResponse(sMessage);
-        }*/
+        /* if (userId.isEmpty() || userId.length() < 5) {
+         * String sMessage = "Error invoking getFund - Invalid fund Id parameter";
+         * // return createErrorResponse(sMessage);
+         * } */
         //try {
         user = UserService.getUserById(Long.parseLong(userId));
         if (user != null) {
@@ -38,10 +38,10 @@ public class RestUserController {
         } else {
             return new ModelAndView("profile/user", "userId", userId);
         }
-        /*} catch (Exception e) {
-            String sMessage = "Error invoking getFund. [%1$s]";
-            //return createErrorResponse(String.format(sMessage, e.toString()));
-        }*/
+        /* } catch (Exception e) {
+         * String sMessage = "Error invoking getFund. [%1$s]";
+         * //return createErrorResponse(String.format(sMessage, e.toString()));
+         * } */
     }
 
     @RequestMapping(value = "/createUser", method = RequestMethod.POST)
@@ -50,16 +50,22 @@ public class RestUserController {
         UserService.createUser(user);
         return new ModelAndView("profile/user", "userObject", user);
     }
+
     @RequestMapping(value = "/myprofile")
     public ModelAndView myProfile(@ModelAttribute("userObject") User user) {
         return new ModelAndView("profile/userprofile", "userObject", user);
+    }
+
+    @RequestMapping(value = "/myprofile/edit", method = RequestMethod.GET)
+    public ModelAndView editUser(@ModelAttribute("userObject") User user) {
+        user = UserService.getUserById(Long.parseLong("1")); //TODO: user hardcoded remove after testing
+        return new ModelAndView("profile/editprofile", "userObject", user);
     }
 
     @RequestMapping(value = "/reset/forgotPassword")
     public ModelAndView forgotpassword() {
         System.out.println("forgotpassword: Passing through...");
 //        return "profile/forgotpassword";
-
         return new ModelAndView("profile/forgotpassword", "message", "");
     }
 
@@ -87,9 +93,9 @@ public class RestUserController {
     }
 
     @RequestMapping(value = "/reset/setNewPassword", method = RequestMethod.POST)
-    public String setNewPassword(/*@RequestParam(value = "userObject")*/ /*@ModelAttribute("userObject") User user,*/
-                                 @RequestParam(value = "passwordResetString") String passwordResetString,
-                                 @RequestParam(value = "password") String password) {
+    public String setNewPassword(/* @RequestParam(value = "userObject") */ /* @ModelAttribute("userObject") User user, */
+            @RequestParam(value = "passwordResetString") String passwordResetString,
+            @RequestParam(value = "password") String password) {
         User user = UserService.getUserByResetString(passwordResetString);
 
         user.setPassword(password);
