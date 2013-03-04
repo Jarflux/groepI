0fe04a7190a904a599d78823b8b2f85a65361cd4
Mitch Van Daele <mitch842@hotmail.com>
2013-03-04 21:27:03 +0100

    Android login

---
+++ TrippieApp/src/com/model/User.java
@@ -0,0 +1,66 @@
+package com.model;
+
+import java.sql.Timestamp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mitch Va Daele
+ * Date: 4-3-13
+ * Time: 19:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Long dateOfBirth;
+
+    public User(Long id, String name, String email, String password, Long dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Long dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
---
+++ TrippieApp/src/com/utils/DateUtil.java
@@ -0,0 +1,40 @@
+package com.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static Long dateToLong(int day, int month, int year, int hour, int minute, int second) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day, hour, minute, second);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime().getTime();
+    }
+
+    public static Long dateStringToLong(String dateString, String timeString) {
+        int day = 0, month = 0, year = 0, hour = 0, min = 0, sec = 0;
+
+        if (dateString != null) {
+            String[] dateArray = dateString.split("-");
+            day = Integer.parseInt(dateArray[2]);
+            month = Integer.parseInt(dateArray[1]);
+            year = Integer.parseInt(dateArray[0]);
+        }
+        if (timeString != null) {
+            String[] timeArray = timeString.split(":");
+            hour = Integer.parseInt(timeArray[0]);
+            min = Integer.parseInt(timeArray[1]);
+            sec = Integer.parseInt(timeArray[2]);
+        }
+        Long date = DateUtil.dateToLong(day, month, year, hour, min, sec);
+        return date;
+    }
+
+    public static Date longToDate(Long date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(date);
+        return cal.getTime();
+    }
+}
