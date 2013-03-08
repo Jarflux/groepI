0a2a615e03efa8c7cdc3301ffc49e1fba5e5668f
Mitch Van Daele <mitch842@hotmail.com>
2013-03-08 22:40:38 +0100

    /

---
+++ src/main/java/be/kdg/groepi/controller/AndroidRestController.java
@@ -0,0 +1,28 @@
+package be.kdg.groepi.controller;
+
+import be.kdg.groepi.model.TripInstance;
+import be.kdg.groepi.service.TripInstanceService;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mitch Va Daele
+ * Date: 8-3-13
+ * Time: 22:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping("android")
+public class AndroidRestController {
+}
