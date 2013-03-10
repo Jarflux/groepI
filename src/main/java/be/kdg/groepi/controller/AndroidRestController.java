5277d701f496f09a34187af58cbe12291fd6a155
Mitch Van Daele <mitch842@hotmail.com>
2013-03-10 01:08:36 +0100

    AndroidController

--- src/main/java/be/kdg/groepi/controller/AndroidRestController.java
+++ src/main/java/be/kdg/groepi/controller/AndroidRestController.java
@@ -2,16 +2,17 @@ package be.kdg.groepi.controller;
 
 import be.kdg.groepi.model.TripInstance;
 import be.kdg.groepi.service.TripInstanceService;
+import com.google.gson.Gson;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,4 +26,15 @@ import java.util.List;
 @Controller
 @RequestMapping("android")
 public class AndroidRestController {
+    @RequestMapping(value = "/showUserTripParticipations/{userId}", method = RequestMethod.GET)
+    public void getUserTripParticipations(@PathVariable(value = "userId") String userId, HttpServletResponse response) {
+        Gson gson = new Gson();
+        List<TripInstance> trips = TripInstanceService.getTripInstancesByUserId(Long.parseLong(userId));
+        String json = gson.toJson(trips);
+        try {
+            response.getWriter().print(json);
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 }
