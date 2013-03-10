23e5c8d3f72e1055f26f040f0f95e59aba65175f
Mitch Van Daele <mitch842@hotmail.com>
2013-03-10 17:33:46 +0100

    Hibernate & test aanpassingen Answer & AnswerInstance (AnswerInstanceTest dient nog aangepast te worden)

--- src/main/java/be/kdg/groepi/controller/AndroidRestController.java
+++ src/main/java/be/kdg/groepi/controller/AndroidRestController.java
@@ -1,8 +1,11 @@
 package be.kdg.groepi.controller;
 
+import be.kdg.groepi.model.Trip;
 import be.kdg.groepi.model.TripInstance;
 import be.kdg.groepi.service.TripInstanceService;
+import be.kdg.groepi.utils.ExclusionStrategyUtil;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -28,10 +31,10 @@ import java.util.List;
 public class AndroidRestController {
     @RequestMapping(value = "/showUserTripParticipations/{userId}", method = RequestMethod.GET)
     public void getUserTripParticipations(@PathVariable(value = "userId") String userId, HttpServletResponse response) {
-        Gson gson = new Gson();
-        List<TripInstance> trips = TripInstanceService.getTripInstancesByUserId(Long.parseLong(userId));
-        String json = gson.toJson(trips);
         try {
+            List<TripInstance> trips = TripInstanceService.getTripInstancesByUserId(Long.parseLong(userId));
+            Gson gson = new GsonBuilder().setExclusionStrategies(new ExclusionStrategyUtil(Trip.class)).serializeNulls().create();
+            String json = gson.toJson(trips);
             response.getWriter().print(json);
         } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
